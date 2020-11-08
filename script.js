f4d1bdd3cd1af2a402f3c892eb1909069add0b87
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-07 19:27:29 -0600

    updating js

--- script.js
+++ script.js
@@ -1,118 +1,49 @@
-//select all elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreDiv");
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
 
-// create questions
-let questions = [
-    {
-        question : "What does API stand for?",
-        choiceA : "artificial programming interface",
-        choiceB : "application programming interlace",
-        choiceC : "application programming interface",
-        choiceD : "artificial property interface",
-        correct : "C"
-    },{
-        question : "What does CSS stand for?",
-        choiceA : "cascading style sheets",
-        choiceB : "cascading sheets style",
-        choiceC : "computing style sheets",
-        choiceD : "API",
-        correct : "A"
-    },{
-        question : "What does HTML stand for?",
-        choiceA : "hypertext markup link",
-        choiceB : "hyperlink text markup",
-        choiceC : "hotmail",
-        choiceD : "hypertext markup language",
-        correct : "D"
-    },{
-        question : "What is JavaScript?",
-        choiceA : "used to create a link",
-        choiceB : "a coding language for Android decvices",
-        choiceC : "used to create dynamic web applications",
-        choiceD : "artificial property interface",
-        correct : "C"
-    },{
-        question : "What is a variable?",
-        choiceA : "numbers, strings, Booleans",
-        choiceB : "static websites",
-        choiceC : "application programming interface",
-        choiceD : "a repository",
-        correct : "A"
-    }
-];
+let shuffledQuestions, currentQuestionIndex
 
-// create variables
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
+startButton.addEventListener('click', startGame)
 
-// render a question
-function renderQuestion(){
-    let q = questions[runningQuestion];
-
-    question.innerHTML = "<p>"+ q.question +"</p>";
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
-
-
-
-
-
-
-const questionTime = 10;
-const gaugeWidth = 150;
-let count = 0;
-const gaugeProgressUnit = gaugeWidth/questionTime;
-
-function counterRender() {
-    if (count <= questionTime ) {
-        counter.innerHTML = count;
-        timeGauge.style.width = gaugeProgressUnit * count + "px" ;
-        count ++;
-    } else {
-        count = 0;
-        answerIsWrong();
-        if (runningQuestionIndex < lastQuestionIndex){
-            runningQuestionIndex++;
-            questionRender();
-        } else { clearInterval(TIMER);
-                scoreRender();
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+    
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
         }
+        button.addEventListener('click', selectAnswer)
     }
+    );
+
 }
-let lastQuestionIndex = questions.lenght - 1; 
-let runningQuestionIndex = 0;
+function selectAnswer(e) {
 
-function renderQuestion() {
-    let q = questions[runningQuestionIndex];
-    question.innerHTML = "<p>" + q.question+ "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
+}
 
-function progressRender(){ 
-    for(let qIndex = 0; qIndex <= lastQuestionIndex; qIndex++){
-        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+
+const questions = [
+    {
+        question: "   ",
+        answers: [
+            {}
+        ]
     }
-}
-function answerIsCorrect() {
-    document.getElementById(runningQuestionIndex).style.backgroundColor = "green"
-}
-function answerIsWrong() {
-    document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
-}
-
-let TIMER = 
-    setInterval(counterRender, 1000);
-    stop running: setInterval()
-    clearInterval( );
+]
